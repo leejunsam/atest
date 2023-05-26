0824382e7ccfac05cfd6b773764714037fc7ff87
Administrator <Administrator@DESKTOP-2VLVNHN>
2023-05-26 10:30:14 +0900

    first_1

--- TestGit/src/main/java/com/web/root/HomeController.java
+++ TestGit/src/main/java/com/web/root/HomeController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Handles requests for the application home page.
  */
 @Controller
-public class HomeController {//f
+public class HomeController {//ff
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
